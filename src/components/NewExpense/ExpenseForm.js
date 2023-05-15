f086669e2a8d988f2516a7fc4086f71396fc2c62
Miftakhul Ulum Khasanah <115676049+miftaaaaaa@users.noreply.github.com>
2023-05-15 20:39:40 +0700

    Update ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -151,7 +151,7 @@ const ExpenseForm = (props) => {
       
       <div className='new-expense__actions'>
         <button type="button" onClick={props.onCancel}>Cancel</button>
-        <button type='submit'>Add Expense</button>
+        <button type='submit'>Tambah Data</button>
       </div>
     </form>
   );
